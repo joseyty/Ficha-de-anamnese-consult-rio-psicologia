402bd54ecaa1e2474b05ef26a5ce5500d285bdc5
Igor jose <lmelo7262@gmail.com>
2026-05-12 17:35:24 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 function adicionarButton(){
 
-    let realizado = "Adicionado com sucesso!";
+
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let sexo = document.getElementById("sexo").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let endereco = document.getElementById("endereco").value;
+
+
+    let realizado = "Enviadocom sucesso!";
    document.getElementById("mensagem").textContent = realizado;
-   
+     document.getElementById("mensagem").style.color = "green";
 
    
+
  }
+
+
+
+
+
